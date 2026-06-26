f450d498aceba364500e86934e8d240212ba56b6
Karthikeya316 <pkarthikeya112007@gmail.com>
2026-06-26 21:04:16 +0000

    Resolved few bugs and added some reliable comments for better understanding of the workflow

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ client=Groq(api_key=GROQ_API_KEY)
 #-------------------------------------------------------------------------------
 #global variables
 #-------------------------------------------------------------------------------
-document_folder="documents" #folder contains pdf and txt files
+document_folder="documents"#folder contains pdf and txt files
+documents=[] #store complete documents
 chunks=[] #store chunks of document
 chat_history=[] #store chat history
 vectorizer=None #Vectorize the doc
@@ -30,13 +31,16 @@ chunk_vectors=None
 #---------------------------------------------------------------------------------
 #load documents
 #---------------------------------------------------------------------------------
-def load_documents():
+def load_documents():  #os.listdir() returns a list of all filenames in the folder
     for file_name in os.listdir(document_folder):   #loop through all the files
+        #Joins the folder path and filename into a full path — e.g. docs/report.pdf
         file_path=os.path.join(document_folder,file_name)  #create a full path
         text=""  
         if file_name.endswith(".txt"):   #read .txt file
             with open(file_path,"r",encoding='utf-8') as file:
                 text=file.read()
+            #create a PdfReader object, loop through every page, extract text from each page, and append it to text.
+            #The if page_text check skips blank/image-only pages that return None         
         elif file_name.endswith(".pdf"):  #read .pdf files
             reader=PdfReader(file_path)   
             for page in reader.pages:     #loop through all the pages of PDF
@@ -44,6 +48,6 @@ def load_documents():
                 if page_text:
                     text+=page_text    #extract text from each
         if text.strip():   #check if there is any text
-            document_folder.append({"filename":file_name,"text":text})
+            documents.append({"filename":file_name,"text":text})
                               
             
